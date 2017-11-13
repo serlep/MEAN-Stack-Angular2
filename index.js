274574f774fb36b8096540e4a9242d87ea15becd
serlep_s <sherab.serlep@epitech.eu>
2017-11-13 21:58:30 +0100

    server-side-register-setup

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const mongoose = require('mongoose');
 const config = require('./config/database');
 const path = require('path');
+const authentication = require('./routes/authentication')(router);
+const bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
 const option = {
 	useMongoClient: true
 };
+
 mongoose.connect(config.uri, option,(err) => {
 
 	if(err) {
@@ -17,7 +21,10 @@ mongoose.connect(config.uri, option,(err) => {
 	}
 });
 
+app.use(bodyParser.urlencoded({extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/client/dist/'));
+app.use('/authentication', authentication);
 
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname + '/client/dist/index.html'));
